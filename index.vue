db612f09dfbddaec31b83e9ed34fa3f90d12d283
tokhtar.aubakirov <tokhtar.aubakirov@ffins.kz>
2024-02-14 14:15:42 +0600

    fix

--- index.vue
+++ index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import {reactive} from "vue"
+import {reactive, computed} from "vue"
 import FFForm from "@/components/FFUI/FFForm.vue"
 import FFFieldset from "@/components/FFUI/FFFieldset.vue"
 import FFInput from "@/components/FFUI/FFInput.vue"
@@ -37,32 +37,31 @@ const objectSchema = z.object({
     .nonempty("Phone is required")
     .regex(/^\+7 \(\d{3}\) \d{3} \d{2} \d{2}$/, "Invalid phone number format")
     .min(16, "Phone number is too short"),
-  iin: z
-    .string()
-    .nonempty("IIN is required")
-    .length(14, "IIN must be exactly 12 digits")
+  iin: z.string().nonempty("IIN is required").length(14, "IIN must be exactly 12 digits")
 })
 
 const arraySchema = z.array(objectSchema)
 
-const {data, errors, isValid, validate, clearErrors, getError} = useValidation(arraySchema, dataTest)
+const {data, isValid, errors, isPassedValidation, submitIsClicked, validate, debouncedValidateField} = useValidation(
+  arraySchema,
+  objectSchema,
+  dataTest
+)
 
 const submitForm = () => {
   validate()
-  if (isValid) {
+  const validationResult = arraySchema.safeParse(dataTest)
+  if (validationResult.success) {
     function normalizeData(data: IDataTest[]) {
       return data.map((item: IDataTest) => ({
         phone: phoneNumberFormatter(item.phone),
         iin: item.iin
       }))
     }
-
-    console.log(normalizeData(data.value as IDataTest[]))
+    console.log(normalizeData(dataTest))
+  } else {
+    console.log("Did not pass validation")
   }
-}
-
-const hasError = (field: string, index: number) => {
-  return getError(`${index}.${field}`) !== undefined
 }
 </script>
 
@@ -70,17 +69,45 @@ const hasError = (field: string, index: number) => {
   <div class="flex justify-center items-center text-white h-[400px]">
     <template v-for="(form, index) in data" :key="index">
       <FFForm>
-        <FFFieldset title="Телефон" :required="hasError('phone', index)">
-          <FFInput v-maska data-maska="+7 (###) ### ## ##" id="phone" name="phone" v-model="form.phone" type="tel" />
-        </FFFieldset>
-        {{ getError(`${index}.phone`)?.message }}
+        <div class="flex flex-col">
+          <FFFieldset
+            title="Телефон"
+            :required="submitIsClicked && !form.phone"
+            :isError="!!form.phone && !isPassedValidation[index]?.phone">
+            <FFInput
+              v-maska
+              data-maska="+7 (###) ### ## ##"
+              id="phone"
+              name="phone"
+              v-model="form.phone"
+              type="tel"
+              @input="debouncedValidateField(index, 'phone')" />
+          </FFFieldset>
+          <span v-show="errors[index]?.phone?.message" class="mt-1 text-red-400">
+            {{ errors[index]?.phone?.message }}
+          </span>
+        </div>
 
-        <FFFieldset title="ИИН" :required="hasError('iin', index)">
-          <FFInput v-maska data-maska="#### #### ####" id="iin" name="iin" v-model="form.iin" type="text" />
-        </FFFieldset>
-        {{ getError(`${index}.iin`)?.message }}
+        <div class="flex flex-col">
+          <FFFieldset
+            title="ИИН"
+            :required="submitIsClicked && !form.iin"
+            :isError="!!form.iin && !isPassedValidation[index]?.iin">
+            <FFInput
+              v-maska
+              data-maska="#### #### ####"
+              id="iin"
+              name="iin"
+              v-model="form.iin"
+              type="text"
+              @input="debouncedValidateField(index, 'iin')" />
+          </FFFieldset>
+          <span v-show="errors[index]?.iin?.message" class="mt-1 text-red-400">
+            {{ errors[index]?.iin?.message }}
+          </span>
+        </div>
 
-        <FFButton @click="submitForm" title="Нажми броо" />
+        <FFButton @click="submitForm" title="pick me puta madra" />
       </FFForm>
     </template>
   </div>
--- useValidation.ts
+++ useValidation.ts
@@ -1,31 +1,91 @@
-import { ref, computed } from "vue";
-import * as zod from "zod";
+import {ref, computed} from "vue"
+import {z} from "zod"
+import {useI18n} from "vue-i18n"
+import { useDebounce } from "@/composables/useDebounce"
 
-export function useValidation<T>(validationSchema: zod.ZodSchema<T>, initialData: T | T[]) {
-  const data = ref(initialData) as { value: T | T[] };
-  const errors = ref<zod.ZodIssue[]>([]);
-  const isValid = computed(() => errors.value.length === 0);
+export function useValidation<T>(
+  validationArraySchema?: z.ZodArray<any>,
+  validationObjectSchema?: z.ZodObject<any>,
+  initialData?: T | T[]
+) {
+  const data = ref(initialData) as {value: T | T[]}
+  const errors = ref<z.ZodIssue[]>([])
+  const submitIsClicked = ref<boolean>(false)
+  const isValid = computed(() => errors.value.length === 0)
 
+  let isPassedValidation = ref<{[key: string]: boolean}[]>([])
+  const {t} = useI18n()
+
+  const validateField = (index: number, field: string) => {
+    const dataItem = data.value[index][field]
+    const fieldSchema = validationObjectSchema.shape[field]
+    const result = fieldSchema.safeParse(dataItem)
+
+    if (!errors.value[index]) {
+      errors.value[index] = {}
+    }
+
+    errors.value[index][field] = result.success
+      ? undefined
+      : {
+          ...(result.error && result.error.issues && result.error.issues[0]),
+          message: t(`app.form.validation.${result.error.issues[0].message}`)
+        }
+
+    if (!isPassedValidation.value[index]) {
+      isPassedValidation.value[index] = {}
+    }
+
+    if (isPassedValidation.value[index][field] === undefined) {
+      isPassedValidation.value[index][field] = false
+    }
+
+    isPassedValidation.value[index][field] = result.success
+  }
+
+  const debouncedValidateField = useDebounce(validateField, 100)
 
   const validate = () => {
-    const result = validationSchema.safeParse(data.value);
-    errors.value = result.success ? [] : result.error.issues;
-  };
+    submitIsClicked.value = true
+    errors.value = []
+
+    if (Array.isArray(data.value)) {
+      data.value.forEach((dataItem, index) => {
+        const result = validationArraySchema?.safeParse(dataItem)
+
+        if (!result.success) {
+          errors.value.push(...result.error.issues)
+        }
+
+        Object.keys(dataItem).forEach((field) => {
+          validateField(index, field)
+        })
+      })
+    } else {
+      const result = validationObjectSchema?.safeParse(data.value)
+
+      if (!result.success) {
+        errors.value.push(...result.error.issues)
+      }
+
+      Object.keys(data.value).forEach((field) => {
+        validateField(0, field)
+      })
+    }
+  }
 
   const clearErrors = () => {
-    errors.value = [];
-  };
-
-  const getError = (path: string) => {
-    return errors.value.find(issue => issue.path.join(".") === path);
-  };
+    errors.value = []
+  }
 
   return {
     data,
     errors,
+    isPassedValidation,
+    submitIsClicked,
     isValid,
     validate,
     clearErrors,
-    getError,
-  };
+    debouncedValidateField,
+  }
 }
